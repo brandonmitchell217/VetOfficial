7f9674baab35d37904c99965515ae50077881652
Brandon Mitchell <brandonmitchell217@gmail.com>
2021-05-08 00:03:14 -0400

    First Commit. Almost finalized version of official site. Animations added, no timeline setup.

---
+++ animation.js
@@ -0,0 +1,112 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const mediaQuery4 = window.matchMedia(
+  "(min-width: 320px)",
+  "(max-width: 767px)"
+);
+const mediaQuery5 = window.matchMedia("(min-width: 768px)");
+
+const timeline1 = gsap.timeline({ defaults: { y: "-100vh" } });
+timeline1
+  .from(".navLogo", { duration: 1 })
+  .from(".fullNavList li", { duration: 0.45, stagger: 0.5 });
+if (mediaQuery5.matches) {
+  gsap.from(".card1", { duration: 4, delay: 3.2, opacity: "0" });
+  gsap.from(".card2", { duration: 4, delay: 3.6, opacity: "0" });
+} else if (mediaQuery4.matches) {
+  gsap.from(".card1", { duration: 4, delay: 1.4, opacity: "0" });
+  gsap.from(".card2", { duration: 4, delay: 1.6, opacity: "0" });
+}
+
+gsap.from(".musicWrap", {
+  duration: 1,
+  opacity: "0",
+  ease: "power1.in",
+  scrollTrigger: {
+    trigger: ".musicWrap",
+    start: "top 90%",
+  },
+});
+// About
+gsap.from(".aboutTitle", {
+  duration: 1,
+  opacity: "0",
+  y: "-75%",
+  ease: "bounce",
+  scrollTrigger: {
+    trigger: ".aboutTextWrapper",
+    start: "top bottom",
+  },
+});
+gsap.from(".aboutTextInner", {
+  duration: 1,
+  opacity: "0",
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".aboutContent",
+    start: "center 75%",
+  },
+});
+
+// Media
+gsap.from(".mediaTitle", {
+  duration: 1,
+  opacity: "0",
+  ease: "power1.in",
+  scrollTrigger: {
+    trigger: "#about",
+    start: "bottom bottom",
+  },
+});
+gsap.from(".mediaVid:nth-child(1)", {
+  duration: 1,
+  x: "-100%",
+  scrollTrigger: {
+    trigger: ".mediaVid:nth-child(1)",
+    start: "top bottom",
+  },
+});
+gsap.from(".mediaVid:nth-child(2)", {
+  duration: 1,
+  x: "100%",
+  scrollTrigger: {
+    trigger: ".mediaVid:nth-child(2)",
+    start: "top bottom",
+  },
+});
+gsap.from(".mediaVid:nth-child(3)", {
+  duration: 1,
+  x: "-100%",
+  scrollTrigger: {
+    trigger: ".mediaVid:nth-child(3)",
+    start: "top bottom",
+  },
+});
+gsap.from(".mediaVid:nth-child(4)", {
+  duration: 1,
+  x: "100%",
+  scrollTrigger: {
+    trigger: ".mediaVid:nth-child(4)",
+    start: "top bottom",
+  },
+});
+// Photos
+gsap.from(".instaOuter", {
+  duration: 1,
+  opacity: "0",
+  ease: "power1.in",
+  scrollTrigger: {
+    trigger: ".videosWrap",
+    start: "bottom bottom",
+  },
+});
+// Contact
+gsap.from("#contact", {
+  duration: 1,
+  opacity: "0",
+  ease: "power1.in",
+  scrollTrigger: {
+    trigger: ".instaOuter",
+    start: "bottom bottom",
+  },
+});
---
+++ main1.js
@@ -0,0 +1,42 @@
+// gsap.registerPlugin(CSSRulePlugin);
+// Burger Menu
+const burgerOpen = document.querySelector(".burgerOpen");
+const burgerClose = document.querySelector(".burgerClose");
+const navList = document.querySelector("#navList");
+const mediaQuery1 = window.matchMedia("(max-width: 320px)");
+const mediaQuery2 = window.matchMedia(
+  "(min-width: 321px)",
+  "(max-width: 375px)"
+);
+const mediaQuery3 = window.matchMedia(
+  "(min-width: 376px)",
+  "(max-width: 425px)"
+);
+
+burgerOpen.addEventListener("click", () => {
+  navList.style.transform = "translateX(0)";
+  burgerClose.style.display = "block";
+  burgerOpen.style.transform = "scale(0)";
+  if (mediaQuery1.matches) {
+    gsap.to(".navLogo", { scale: "1.5", x: "-32vw", y: "16vh" });
+  }
+  if (mediaQuery2.matches) {
+    gsap.to(".navLogo", { scale: "1.65", x: "-34vw", y: "16vh" });
+  }
+  if (mediaQuery3.matches) {
+    gsap.to(".navLogo", { scale: "1.75", x: "-36vw", y: "16vh" });
+  }
+});
+burgerClose.addEventListener("click", () => {
+  navList.style.transform = "translateX(-300%)";
+  burgerClose.style.display = "none";
+  burgerOpen.style.transform = "scale(1)";
+  gsap.to(".navLogo", { scale: "1", x: "0", y: "0" });
+});
+
+window.addEventListener("keydown", function (event) {
+  if (event.key == "Escape") {
+    navList.style.transform = "translateX(-300%)";
+    burgerOpen.style.display = "block";
+  }
+});
